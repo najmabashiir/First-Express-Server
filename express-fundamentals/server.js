ae60848d7fae050de0a03a1bc952274ba8c41f62
najmabashiir <najaxbashiirali>
2026-01-19 19:56:56 +0300

    using http module code
    Signed-off-by: najmabashiir <najaxbashiirali>

--- express-fundamentals/server.js
+++ express-fundamentals/server.js
@@ -1,12 +1,29 @@
-const express= require('express');
-const server=express();
-const PORT=3000;
+// const express= require('express');
+// const server=express();
+// const PORT=3000;
 
-// basic route
-server.get('/',(req,res)=>{
-    res.send("hello najma")
+// // basic route
+// server.get('/',(req,res)=>{
+//     res.send("hello najma")
+// });
+// // start server
+// server.listen(PORT,()=>{
+//     console.log(`server is running at http://localhost:${PORT}`)
+// })
+
+// http module code
+const http= require('http');
+const PORT=3001;
+
+const server=http.createServer((req,res)=>{
+    if(req.method==='GET' && req.url==='/'){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('hello najma with express fundamentals');
+    }else{
+        res.writeHead(404,{'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    }
 });
-// start server
 server.listen(PORT,()=>{
-    console.log(`server is running at http://localhost:${PORT}`)
-})
+    console.log(`server is running at http://localhost:${PORT}`);
+});
